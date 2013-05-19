c3a89f93d1f9342b74073af5e03ef1c9b8496565
Brandon Townsend <btownsen@dapperhat.net>
2013-05-19 16:09:18 -0700

    added missing files

---
+++ BladeQuest/src/bladequest/UI/MainMenu/MainMenuState.java
@@ -0,0 +1,37 @@
+package bladequest.UI.MainMenu;
+
+import bladequest.world.Global;
+
+
+public class MainMenuState
+{
+	MainMenuState prevState;
+	public final void setPreviousState(MainMenuState prevState)
+	{
+		this.prevState = prevState;
+	}
+	public void cancelToPrevState()
+	{
+		Global.menu.cancelToState(prevState);
+	}
+	
+	public void changeStateTo(MainMenuState state) {}
+	public void onSwitchedTo(MainMenuState prevState) {}
+
+	public void update() {}
+	public void render() {}
+	
+	public void handleClosing(){}
+	
+	public void changeToRoot()	{}
+	public void changeToItemSelect() {}
+	
+	public void onFling(float velocityX, float velocityY) {}
+	public void backButtonPressed() {}
+	public void touchActionUp(int x, int y) {}
+	public void touchActionMove(int x, int y) {}
+	public void touchActionDown(int x, int y) {}
+	
+
+
+}
