d98fe20ea8502ee2817911c3460e9aefaace4b22
p4r4digm <p4r4digm@dapperhat.net>
2013-08-03 04:31:33 -0700

    added the new dropbox to the spudquest board to great effect

--- BladeQuest/src/bladequest/spudquest/UI.java
+++ BladeQuest/src/bladequest/spudquest/UI.java
@@ -1,6 +1,8 @@
 package bladequest.spudquest;
 
+import android.graphics.Rect;
 import bladequest.UI.DropBox;
+import bladequest.UI.MenuPanel;
 import bladequest.graphics.Scene;
 import bladequest.world.Global;
 
@@ -14,32 +16,55 @@ public class UI
 		backdrop = Global.scenes.get("sqbd");
 		backdrop.load();
 		
-		//board = new DropBox(x, y, width, height, rows, columns)
+		buildDropBox();
+	}
+	
+	private void buildDropBox()
+	{
+		board = new DropBox(0, 0, Global.vpWidth, Global.vpHeight);
+		board.drawFrame = false;
+		
+		for(int y = 0; y < 4; ++y)
+			for(int x = 0; x< 4; ++x)
+				board.addPanelRect(new Rect(10 + 60*x, 50 + 60*y, 10 + 60*x + 48, 50 + 60*y + 48));
+		
+		
+		for(int y = 0; y < 2; ++y)
+			for(int x = 0; x< 4; ++x)
+				board.addPanelRect(new Rect(246 + 50*x, 186 + 50*y, 246 + 50*x + 48, 186 + 50*y + 48));
+		
+		for(int i = 0; i < 24; ++i)
+		{
+			MenuPanel mp = new MenuPanel(0, 0, 48, 48);
+			board.addPanel(mp, i);
+		}
+		
 	}
 	
 	public void update()
 	{
-		
+		board.update();
 	}
 	
 	public void render()
 	{
 		backdrop.render();
+		board.render();
 	}
 	
 	public void touchActionMove(int x, int y)
 	{
-		
+		board.touchActionMove(x, y);
 	}
 	
 	public void touchActionUp(int x, int y)
 	{
-		
+		board.touchActionUp(x, y);
 	}
 	
 	public void touchActionDown(int x, int y)
 	{
-		
+		board.touchActionDown(x, y);
 	}
 	
 
